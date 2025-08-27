cb3047292e7bd45c9b377af2178bcc319f58c1fd
yoshioka0101 <yuuki0205nano@gmail.com>
2025-08-28 02:48:16 +0900

    init commit

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/toybox97/go-sse
+
+go 1.24.5
---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <h1>Vue SSE Frontend</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
